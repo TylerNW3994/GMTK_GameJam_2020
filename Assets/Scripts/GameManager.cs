89039736da2b38bbf2968d41628e239e411ff972
TylerNW3994 <TylerNW3994@gmail.com>
2020-07-10 16:47:25 -0400

    Created scripts

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public string[] orderTypes;
+    public Image[] orderImages;
+    public int[] orderTimes;
+
+    public Order[] currentOrders;
+
+    public static GameManager instance;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
